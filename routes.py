8dd8bbbd41131758937a744343809edbdf9a8ddd
Akhila Sharma <akhilasharma17@gmail.com>
2024-05-09 15:00:41 +1200

    functioning plant page

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
-from flask import Flask, template
+from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
@@ -6,16 +7,36 @@ app = Flask(__name__)
 # homepage
 @app.route('/')
 def homepage():
-    return template("home.html")
+    return render_template("home.html")
 
 
-# plants page
+'''
+# all plants page
+@app.route('/plant/all_plant')
+def all_plant():
+    connect = sqlite3.connect('plant.db')
+    cursor = connect.cursor()
+    cursor.execute("SELECT * FROM Plant ORDER BY id", (id,))
+    all_plant = cursor.fetchall()
+    return render_template('plant.html', plant=all_plant)
+'''
+
+
+# individual plants page
 @app.route('/plant/<int:id>')
 def plant(id):
-    return template("plant.html")
+    connect = sqlite3.connect('plant.db')
+    cursor = connect.cursor()
+    cursor.execute("SELECT * FROM Plant WHERE id=?", (id,))
+    plant = cursor.fetchone()
+    return render_template('plant.html', plant=plant)
 
 
 # about/contact information page
 @app.route('/contact')
 def contactpage():
-    return template("contact.html")
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
