7f0145f98a87d13940530721913d518671605f85
Akhila Sharma <akhilasharma17@gmail.com>
2024-06-10 14:08:11 +1200

    'all plants' page displays links to each individual plant

--- routes.py
+++ routes.py
@@ -10,16 +10,15 @@ def homepage():
     return render_template("home.html")
 
 
-'''
 # all plants page
-@app.route('/plant/all_plant')
+@app.route('/all_plant')
 def all_plant():
     connect = sqlite3.connect('plant.db')
     cursor = connect.cursor()
-    cursor.execute("SELECT * FROM Plant ORDER BY id", (id,))
-    all_plant = cursor.fetchall()
-    return render_template('plant.html', plant=all_plant)
-'''
+    cursor.execute("SELECT * FROM Plant ORDER BY id;")
+    plants = cursor.fetchall()
+    connect.close()
+    return render_template('all_plant.html', plant=plants)
 
 
 # individual plants page
@@ -30,10 +29,14 @@ def plant(id):
     cursor.execute("SELECT * FROM Plant WHERE id=?", (id,))
     plant = cursor.fetchone()
     # planting instruction
-    cursor.execute("SELECT * FROM Planting_instruction WHERE id=?", (id,))
+    cursor.execute("SELECT * FROM Planting_instruction WHERE id=?;", (id,))
     instruction = cursor.fetchone()
-    return render_template('plant.html', plant=plant,
-                           planting_instruction=instruction)
+    connect.close()
+    if '<int:id' == 'NULL':
+        return render_template('404.html')
+    else:
+        return render_template('plant.html', plant=plant,
+                               planting_instruction=instruction)
 
 
 # about/contact information page
@@ -42,6 +45,7 @@ def contactpage():
     return render_template("contact.html")
 
 
+'''
 # triangles
 @app.route('/triangle/<int:size>')
 def triangle_pattern(size):
@@ -49,6 +53,7 @@ def triangle_pattern(size):
     for i in range(size):
         triangle += (i+1)*'*' + "<br>"
     return (triangle)
+'''
 
 
 if __name__ == "__main__":
