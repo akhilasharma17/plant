e39a032509b5879f5850d03dbf8bb844a4d606d0
Akhila Sharma <akhilasharma17@gmail.com>
2024-08-22 08:44:38 +1200

    wrote initial css for image gallery in all plant page

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort, request
 import sqlite3
 
 app = Flask(__name__)
@@ -26,34 +26,47 @@ def all_plant():
 def plant(id):
     connect = sqlite3.connect('plant.db')
     cursor = connect.cursor()
-    cursor.execute("SELECT * FROM Plant WHERE id=?", (id,))
-    plant = cursor.fetchone()
-    # planting instruction
+    # get planting instructions information
     cursor.execute("SELECT * FROM Planting_instruction WHERE id=?;", (id,))
     instruction = cursor.fetchone()
-    connect.close()
-    if '<int:id' == 'NULL':
-        return render_template('404.html')
+    cursor.execute("SELECT * FROM Plant WHERE id=?;", (id,))
+    plant = cursor.fetchone()
+    cursor.execute("SELECT * FROM PlantRegion WHERE regionid=?;", (id,))
+    plantregion = cursor.fetchone()
+    cursor.execute("SELECT * FROM Regionlist WHERE id=?;", (id,))
+    regionlist = cursor.fetchall()
+    if not id:
+        abort(404)
+    # else return plant page
     else:
         return render_template('plant.html', plant=plant,
-                               planting_instruction=instruction)
+                               planting_instruction=instruction,
+                               plantregion=plantregion,
+                               regionlist=regionlist)
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    search_term = request.args.get('search_term', '')
+    connect = sqlite3.connect('plant.db')
+    cursor = connect.cursor()
+    # get search from Plant table
+    result = cursor.execute("SELECT * FROM Plant WHERE name LIKE ?;",
+                            (f"%{search_term}%", )).fetchall()
+    connect.close()
+    return render_template("search.html", result=result,
+                           search_term=search_term)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
 
 
 # about/contact information page
 @app.route('/contact')
 def contactpage():
     return render_template("contact.html")
-
-
-'''
-# triangles
-@app.route('/triangle/<int:size>')
-def triangle_pattern(size):
-    triangle = ""
-    for i in range(size):
-        triangle += (i+1)*'*' + "<br>"
-    return (triangle)
-'''
 
 
 if __name__ == "__main__":
