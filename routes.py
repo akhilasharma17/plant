af243ae60356c5a872d117636f1e9e20f8052dc1
Akhila Sharma <akhilasharma17@gmail.com>
2024-09-24 18:40:16 +1200

    created add to watchlist and remove from watchlist options, and added a db_insert query

--- routes.py
+++ routes.py
@@ -5,7 +5,7 @@ import sqlite3
 
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+app.secret_key = 'k'
 
 
 # homepage and watchlist
@@ -20,7 +20,7 @@ def homepage():
             JOIN Watchlist ON Plant.id = Watchlist.plant_id
             JOIN User ON User.id = Watchlist.user_id
             WHERE User.username = ?;
-        """
+            """
         params.append(username)
         plant = db_query(query, False, params)
     else:
@@ -28,7 +28,56 @@ def homepage():
     return render_template("home.html", plant=plant, username=username)
 
 
-# function with connection to database to avoid redundant code
+@app.route('/add_to_watchlist/<int:plant_id>', methods=['POST'])
+def add_to_watchlist(plant_id):
+    username = session.get('username')
+    if not username:
+        flash("You need to log in to add plants to your watchlist")
+        return redirect('/login')
+    user_query = "SELECT id FROM User WHERE username = ?;"
+    user = db_query(user_query, single=True, params=(username,))
+    if user:
+        user_id = user[0]
+        check_query = """SELECT * FROM Watchlist
+        WHERE user_id = ? AND plant_id = ?;"""
+        existing = db_query(check_query, single=True,
+                            params=(user_id, plant_id))
+        if existing:
+            flash("This plant is already in your watchlist.", "info")
+        else:
+            insert_query = """
+                            INSERT INTO Watchlist(user_id, plant_id)
+                            VALUES (?, ?);"""
+            db_insert(insert_query, params=(user_id, plant_id))
+        flash("Plant added to your watchlist.", "success")
+    else:
+        flash("User not found.", "error")
+    return redirect(f'/plant/{plant_id}')
+
+
+@app.route('/remove_watchlist/<int:plant_id>', methods=['POST'])
+def remove_watchlist(plant_id):
+    # Get the username from the session
+    username = session.get('username')
+    if not username:
+        flash('You need to be logged in to remove plants from your watchlist.',
+              'error')
+        return redirect('/login')
+    # get the user id
+    user_query = "SELECT id FROM User WHERE username = ?"
+    user = db_query(user_query, single=True, params=(username,))
+    if not user:
+        flash('User not found.', 'error')
+        return redirect('/')
+    user_id = user[0]
+    # remove the plant from the user's watchlist
+    remove_query = "DELETE FROM Watchlist WHERE user_id = ? AND plant_id = ?"
+    db_insert(remove_query, params=(user_id, plant_id))
+    flash('Plant removed from your watchlist.', 'success')
+    return redirect('/')
+
+
+# function with connection to query database and to avoid redundant code
 def db_query(query, single=False, params=()):
     conn = sqlite3.connect('plant.db')
     cursor = conn.cursor()
@@ -40,6 +89,15 @@ def db_query(query, single=False, params=()):
         result = cursor.fetchall()
     conn.close()
     return result
+
+
+# function with connection to insert into database
+def db_insert(query, params=()):
+    conn = sqlite3.connect('plant.db')
+    cursor = conn.cursor()
+    cursor.execute(query, params)
+    conn.commit()
+    conn.close()
 
 
 # all plants page
@@ -67,6 +125,7 @@ def plant(id):
     plant_region = db_query(plant_region_query, single=True, params=(id,))
     plant_query = "SELECT * FROM Plant WHERE id=?;"
     plant = db_query(plant_query, single=True, params=(id,))
+    username = session.get('username')
     # check if the id is incorrect so that we can show the 404
     if not plant:
         abort(404)
@@ -74,7 +133,8 @@ def plant(id):
         return render_template('plant.html',
                                planting_instruction=instruction,
                                plant=plant,
-                               plant_region=plant_region)
+                               plant_region=plant_region,
+                               username=username)
 
 
 @app.route('/search', methods=['GET'])
@@ -105,24 +165,18 @@ def signup():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        conn = sqlite3.connect('plant.db')
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM User where username = ?", (username,))
-        existing_user = cursor.fetchone()
+        user_query = "SELECT * FROM User where username = ?"
+        existing_user = db_query(user_query, single=True, params=(username,))
         # check if the user already exists so we don't get repeat usernames
         if existing_user:
-            conn.close()
             flash('Username already exists. Please choose another one.',
                   "error")
             return redirect('/signup')
         # hash the password for extra security
         hashed_password = generate_password_hash(password)
-        cursor.execute("INSERT INTO User (username, password) VALUES (?, ?)",
-                       (username, hashed_password))
-        conn.commit()
-        conn.close()
-        flash("Your account has been created. Please log in.",
-              "success")
+        insert_query = "INSERT INTO User (username, password) VALUES (?, ?)"
+        db_insert(insert_query, params=(username, hashed_password))
+        flash("Your account has been created. Please log in.", "success")
         return redirect('/login')
     return render_template('signup.html')
 
